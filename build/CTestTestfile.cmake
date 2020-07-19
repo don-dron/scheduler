946c27d7281b43590b284827ff28bb26cd70856c
Andrey <ya.andreysavo@gmail.com>
2020-07-19 23:33:29 +0300

    Ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dron/Рабочий стол/scheduler/scheduler
-# Build directory: /home/dron/Рабочий стол/scheduler/scheduler/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
