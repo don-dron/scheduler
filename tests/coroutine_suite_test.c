af4e8c6aa2b9a90ab247077cdeb5ab2f90dc3c54
Andrey <ya.andreysavo@gmail.com>
2020-07-20 00:55:33 +0300

    Add tests

--- tests/coroutine_suite_test.c
+++ tests/coroutine_suite_test.c
@@ -88,11 +88,11 @@ int inner_step_count = 0;
 
 void kek()
 {
+    // nop
 }
 
 void test3Foo()
 {
-    printf("%d\n", 88);
     for (int i = 0; i < 123; ++i)
     {
         coroutine first = create_coroutine_on_stack(kek);
@@ -102,7 +102,6 @@ void test3Foo()
 
         free_coroutine_on_stack(&first);
     }
-    printf("%d\n", 99);
 }
 
 void test3Bar()
@@ -113,10 +112,6 @@ void test3Bar()
         resume(&first);
         suspend();
     }
-    printf("%d\n", 55);
-    // FreeCoroutine(first);
-    printf("%d\n", 77);
-
     free_coroutine_on_stack(&first);
 }
 
@@ -128,240 +123,14 @@ void test3()
         resume(&second);
     }
 
-    printf("%d\n", 66);
     assert(inner_step_count == kSteps);
-    printf("%d == %d\n", inner_step_count, kSteps);
-
     free_coroutine_on_stack(&second);
-}
-
-lf_stack *results;
-
-struct stack_data_node
-{
-    stack_node lst_node;
-    int data;
-};
-typedef struct stack_data_node stack_data_node;
-
-void *worker(void *arg)
-{
-    int i = 1000;
-    long long *int_data;
-    while (i--)
-    {
-        int_data = (long long *)malloc(sizeof(long long));
-        assert(int_data != NULL);
-        *int_data = i;
-        int data;
-        for (int j = 0; j < 100; j++)
-        {
-            stack_data_node *stackNode = (stack_data_node *)malloc(sizeof(stack_data_node));
-            stackNode->data = j;
-            stackNode->lst_node.list_mutex = 0;
-
-            push(results, stackNode);
-        }
-        for (int j = 0; j < 100; j++)
-        {
-            stack_data_node *node = pop(results);
-            if (node != 0)
-            {
-                printf("%d\n", node->data);
-                free(node);
-            }
-        }
-
-        free(int_data);
-    }
-
-    return NULL;
-}
-
-void stack_test()
-{
-    results = create_stack();
-    int nthreads = sysconf(_SC_NPROCESSORS_ONLN);
-    int i;
-
-    pthread_t threads[nthreads];
-    printf("Using %d thread%s.\n", nthreads, nthreads == 1 ? "" : "s");
-    for (i = 0; i < nthreads; i++)
-        pthread_create(threads + i, NULL, worker, NULL);
-
-    for (i = 0; i < nthreads; i++)
-        pthread_join(threads[i], NULL);
-
-    printf("Size = %d\n", results->size);
-
-    free_stack(results);
-}
-
-list *lst;
-
-struct data_node
-{
-    list_node lst_node;
-    int data;
-};
-
-typedef struct data_node data_node;
-
-void list_worker(void *arg)
-{
-    int i = 10000;
-    long long *int_data;
-    while (i--)
-    {
-        int_data = (long long *)malloc(sizeof(long long));
-        assert(int_data != NULL);
-        *int_data = i;
-        int data;
-        for (int j = 0; j < 100; j++)
-        {
-            data_node *nd = (data_node *)malloc(sizeof(data_node));
-            nd->data = j;
-            list_push_front(lst, nd);
-        }
-        for (int j = 0; j < 100; j++)
-        {
-            void *item = list_pop_front(lst);
-
-            if (item != 0)
-            {
-                free(item);
-            }
-        }
-        for (int j = 0; j < 100; j++)
-        {
-            data_node *nd = (data_node *)malloc(sizeof(data_node));
-            nd->data = j;
-
-            list_push_back(lst, nd);
-        }
-        for (int j = 0; j < 100; j++)
-        {
-            void *item = list_pop_back(lst);
-            if (item != 0)
-            {
-                free(item);
-            }
-        }
-
-        free(int_data);
-    }
-}
-
-void list_test()
-{
-    lst = create_list();
-    int nthreads = sysconf(_SC_NPROCESSORS_ONLN);
-    int i;
-
-    pthread_t threads[nthreads];
-    for (i = 0; i < nthreads; i++)
-        pthread_create(threads + i, NULL, list_worker, NULL);
-
-    for (i = 0; i < nthreads; i++)
-        pthread_join(threads[i], NULL);
-
-    free_list(lst);
-}
-
-void list_test1()
-{
-    lst = create_list();
-
-    data_node *nd = (data_node *)malloc(sizeof(data_node));
-    nd->data = 10;
-    list_push_front(lst, nd);
-
-    void *item = list_pop_front(lst);
-
-    if (item != 0)
-    {
-        free(item);
-    }
-
-    nd = (data_node *)malloc(sizeof(data_node));
-    nd->data = 10;
-    list_push_front(lst, nd);
-
-    item = list_pop_front(lst);
-
-    if (item != 0)
-    {
-        free(item);
-    }
-
-    free_list(lst);
-}
-
-int sum = 0;
-int atom = 0;
-
-void c()
-{
-    yield();
-    __atomic_fetch_add(&atom, 1, __ATOMIC_SEQ_CST);
-    yield();
-    sum++;
-    yield();
-    sum++;
-    yield();
-    __atomic_fetch_add(&atom, 1, __ATOMIC_SEQ_CST);
-    yield();
-}
-
-void func()
-{
-    for (int i = 0; i < 100; i++)
-    {
-        submit(c);
-        yield();
-    }
 }
 
 int main()
 {
-    printf("Start\n");
     test1();
-    printf("Test 1 \n");
     test2();
-    printf("Test 2 \n");
     test3();
-    printf("Test 3 \n");
-    printf("Test 4 \n");
-
-    stack_test();
-
-    list_test1();
-    list_test();
-
-    new_scheduler();
-
-    for (int i = 0; i < 100; i++)
-    {
-        spawn(func);
-        spawn(func);
-        spawn(func);
-        spawn(func);
-        spawn(func);
-        spawn(func);
-    }
-
-    struct timeval stop, start;
-    gettimeofday(&start, NULL);
-
-    run_scheduler();
-    terminate_scheduler();
-
-    gettimeofday(&stop, NULL);
-
-    unsigned long delta_us = (stop.tv_sec - start.tv_sec) * 1000000 + (stop.tv_usec - start.tv_usec);
-
-    printf("Time microseconds %ld , milliseconds %ld\n",
-           delta_us, delta_us / 1000);
-    printf("atomic %d sum %d\n", atom, sum);
     return EXIT_SUCCESS;
 }
