78482330ca5312dc955285d974bc132d3e82f269
Andrey <ya.andreysavo@gmail.com>
2020-08-23 01:22:03 +0300

    add

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,13 +3,13 @@ cmake -U CMakeLists.txt
 make clean
 make
 
+mkdir -p log
+
 while :
 do
-for var in $(ls bin)
-do
-echo $(./bin/$var)
-done
-
-echo "Done"
-
+    for var in $(ls bin)
+    do
+        rm -f log/log_$var.txt
+        ./bin/$var >> log/log_$var.txt
+    done
 done
